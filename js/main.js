bdf345d75fe95db39e5772472b5ada1ed87a26b4
afolwalex <51683355+afolwalex@users.noreply.github.com>
2019-12-25 22:34:01 +0100

    Updated changes

--- js/main.js
+++ js/main.js
@@ -26,13 +26,13 @@ closeWhatsapp.addEventListener("click", event => {
 // Back to top button
 $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
+      $('.back-to-top').fadeIn('slow')
     } else {
-      $('.back-to-top').fadeOut('slow');
+      $('.back-to-top').fadeOut('slow')
     }
-  });
+})
 
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+$('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo')
     return false;
-  });
+})
